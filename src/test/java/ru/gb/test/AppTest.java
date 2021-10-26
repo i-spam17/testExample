483fafad13b1ab7b24d025a1ffc4719af69c2a1a
i-spam <i_spam@bk.ru>
2021-10-26 23:45:50 +0500

    commit maven project (quickStart)

---
+++ src/test/java/ru/gb/test/AppTest.java
@@ -0,0 +1,20 @@
+package ru.gb.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
